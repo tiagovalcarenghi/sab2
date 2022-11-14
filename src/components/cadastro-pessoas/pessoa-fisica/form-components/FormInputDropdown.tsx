22bf30b913897a71e895afa506d7dbaa386133b5
Tiago Amaral Valcarenghi <tiago.valcarenghi@softplan.com.br>
2022-11-14 14:42:10 -0300

    Cadastro/Pessoas/PF

--- src/components/cadastro-pessoas/pessoa-fisica/form-components/FormInputDropdown.tsx
+++ src/components/cadastro-pessoas/pessoa-fisica/form-components/FormInputDropdown.tsx
@@ -1,3 +1,4 @@
+
 import Select from 'react-select';
 
 
@@ -7,8 +8,20 @@ export interface FormSelectProps {
   options?: any;
 }
 
+const style = {
+  control: (base:any, state:any) => ({
+    ...base,
+    '&:hover': { borderColor: 'orange' , border: '2px solid orange' }, // border style on hover
+    border: '1px solid lightgray', // default border color
+    boxShadow: 'none', // no box-shadow
+    backgroundColor: 'white',
+    height: '41px'
+    
+}),
+};
 
-export const MySelect = ({ onChange, options, value  }: FormSelectProps) => {
+
+export const MySelect = ({ onChange, options, value }: FormSelectProps) => {
 
   const defaultValue = (options: any, value: any) => {
     return options.find((option: any) => option.value === value) || "";
@@ -21,8 +34,9 @@ export const MySelect = ({ onChange, options, value  }: FormSelectProps) => {
         value={defaultValue(options, value)}
         onChange={value => { onChange(value) }}
         options={options}
-        
-         />
+        styles={style}
+
+      />
     </div>
 
   )
